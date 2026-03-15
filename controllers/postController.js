28d678d3605de16748c5be87015c258410352b8d
shaban rasheed <shabanrasheed106@gmail.com>
2026-03-15 19:11:17 +0500

    add post form in dashboard

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,3 +4,21 @@ export const allPosts = async (req, res) => {
   const posts = await Post.find();
   res.json(posts);
 };
+export const addPost = async (req, res) => {
+  res.render("admin/addpost");
+};
+export const addPostData = async (req, res) => {
+  const { ptitle, pbody } = req.body;
+  try {
+    const post = new Post({
+      postTitle: ptitle,
+      postBody: pbody,
+    });
+    await post.save();
+    console.log("pots daved");
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+};
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,4 +1,10 @@
-import { allPosts } from "../controllers/postController.js";
+import {
+  allPosts,
+  addPost,
+  addPostData,
+} from "../controllers/postController.js";
 import express from "express";
 export const postRouter = express.Router();
 postRouter.get("/allposts", allPosts);
+postRouter.get("/addpost", addPost);
+postRouter.post("/addpost", addPostData);
